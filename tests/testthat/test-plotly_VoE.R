249978256a836e779741493bd95e55dcfdd1a15f
Constantin Yves Plessen <34313394+cyplessen@users.noreply.github.com>
2024-12-20 11:49:44 +0100

    minor fix

--- tests/testthat/test-plotly_VoE.R
+++ tests/testthat/test-plotly_VoE.R
@@ -1,15 +1,33 @@
-test_that("plotly_VoE generates an interactive VoE plot", {
+test_that("plotly_VoE returns the correct plot type", {
+
   # Mock data with correct numeric columns
   mock_data <- data.frame(
-    b = as.numeric(c(0.5, 0.3)),
-    pval = as.numeric(c(0.01, 0.05)),
-    k = c(10, 12),
-    set = c("1,2,3", "4,5,6")
+    b = as.numeric(c(0.5, 0.3, 0.3)),
+    pval = as.numeric(c(0.001, 0.05, 0.3)),
+    k = c(10, 12, 11),
+    set = c("1,2,3", "4,5,6", "1")
+  )
+  # Test with interactive = TRUE (should return a plotly object)
+  plot_interactive <- plotly_VoE(
+    data = mock_data,
+    x = "b",
+    y = "pval",
+    colorblind_friendly = TRUE,
+    interactive = TRUE
   )
 
-  # Run the function
-  plot <- plotly_VoE(mock_data, x = "b", y = "pval", colorblind_friendly = T)
+  # Expectations: Check if the plot is a plotly object
+  expect_s3_class(plot_interactive, "plotly")
 
-  # Expectations
-  expect_s3_class(plot, "plotly")
+  # Test with interactive = FALSE (should return a ggplot object)
+  plot_ggplot <- plotly_VoE(
+    data = mock_data,
+    x = "b",
+    y = "pval",
+    colorblind_friendly = TRUE,
+    interactive = FALSE
+  )
+
+  # Expectations: Check if the plot is a ggplot object
+  expect_s3_class(plot_ggplot, "gg")
 })
